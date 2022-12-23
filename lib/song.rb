acf8de717aedf418ddfdf225156e7ebd25794639
jay <jaypifiverr@gmail.com>
2022-12-23 08:53:26 -0500

    commit tree b02368f45d30939bcf9fd0423bf5ffcee7fbb809

---
+++ lib/song.rb
@@ -0,0 +1,46 @@
+require 'pry'
+
+class Song
+
+    @@count = 0
+    @@genres = []
+    @@artists = []
+
+    attr_accessor :name, :artist, :genre
+
+    def initialize(name, artist, genre)
+        @name = name
+        @artist = artist
+        @genre = genre
+        @@count +=1
+        @@artists << artist
+        @@genres << genre
+    end
+
+    def self.count
+        @@count
+    end
+
+    def self.genres
+        @@genres.uniq
+        end
+
+    def self.artists
+        @@artists.uniq
+        end
+
+    def self.genre_count
+        @@genres.group_by { |e| e}.map{|k, v| [k, v.length]}.to_h
+    end
+
+    def self.artist_count
+        @@artists.group_by { |e| e}.map{|k, v| [k, v.length]}.to_h
+    end
+
+end
+
+binding.pry
+
+Song.new("Hi", "Hi Everyone", "pop")
+Song.new("Hi Tim", "Hi Everyone", "rock")
+Song.new("Hello", "Hi Jim", "country")
